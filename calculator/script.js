2348f01bce122328a3c6f2fac4ef280be34ac477
ElenaNam <zaidelsonelena@gmail.com>
2020-09-25 21:05:21 +0400

    feat: add eventhandler

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,5 @@
+// Объявляем переменные для всех кнопок
+
 var numbers = document.querySelectorAll('.number')
 console.log(numbers);
 var operations = document.querySelectorAll('.operator')
@@ -10,6 +12,47 @@ var c = document.getElementById('c')
 console.log(c);
 var result = document.getElementById('result')
 console.log(result);
+//var whatDoButton = document.getElementById('whatDo')
+//console.log(whatDoButton);
+
+
+
+// Обработчики событий
+
+for (var i=0; i<numbers.length; i++){
+    var number = numbers[i];
+    number.addEventListener('click', function(e) {
+        console.log('Клик по кнопке с номером')
+    });
+};
+
+for (var i=0; i<operations.length; i++){
+    var operation = operations[i];
+    operation.addEventListener('click', function(e) {
+        console.log('Клик по кнопке с операцией')
+    });
+};
+
+
+point.addEventListener('click', function(e) {
+    console.log('Клик по кнопке .')
+});
+
+ce.addEventListener('click', function(e) {
+    console.log('Клик по кнопке ce')
+});
+
+
+c.addEventListener('click', function(e) {
+    console.log('Клик по кнопке c')
+});
+
+result.addEventListener('click', function(e) {
+    console.log('Клик по кнопке =')
+});
+
+
+
 
 
 
@@ -25,9 +68,9 @@ function decimal() {
 
 };
 
-function clear() {
+//function clear() {
 
-};
+//};
 
 function whatDo() {
 
