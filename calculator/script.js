3ebcadb1ff1cf3c457e43aa82fce18ad97294e52
ElenaNam <zaidelsonelena@gmail.com>
2020-09-25 20:39:52 +0400

    feat: declare variables

---
+++ calculator/script.js
@@ -0,0 +1,36 @@
+var numbers = document.querySelectorAll('.number')
+console.log(numbers);
+var operations = document.querySelectorAll('.operator')
+console.log(operations);
+var point = document.getElementById('decimal')
+console.log(point);
+var ce = document.getElementById('ce')
+console.log(ce);
+var c = document.getElementById('c')
+console.log(c);
+var result = document.getElementById('result')
+console.log(result);
+
+
+
+function numberPress() {
+
+};
+
+function operation() {
+
+};
+
+function decimal() {
+
+};
+
+function clear() {
+
+};
+
+function whatDo() {
+
+};
+
+
