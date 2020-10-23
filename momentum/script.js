5afafccae9515fc8ccfdf34e59e87d4cd9d41962
ElenaNam <zaidelsonelena@gmail.com>
2020-10-23 13:06:37 +0400

    feat: add error for city

--- momentum/script.js
+++ momentum/script.js
@@ -152,15 +152,20 @@ function setFocus(e) {
 
 async function getWeather() {  
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=8d9aaf89b688cf115332aee8f50199c7&units=metric`;
-  const res = await fetch(url);
-  const data = await res.json(); 
-  //console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
-  weatherIcon.className = 'weather-icon owf';
-  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-  temperature.textContent = `${data.main.temp}°C`;  
-  windspeed.textContent = `скорость ветра ${data.wind.speed}м/с`; 
-  humidity.textContent = `относительная влажность ${data.main.humidity}%`;
-  weatherDescription.textContent = data.weather[0].description;
+  try { 
+    const res = await fetch(url);
+    const data = await res.json();   
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;  
+    windspeed.textContent = `скорость ветра ${data.wind.speed}м/с`; 
+    humidity.textContent = `относительная влажность ${data.main.humidity}%`;
+    weatherDescription.textContent = data.weather[0].description;
+  } catch (error) {
+    city.textContent = 'Город не найден';
+    city.style.background = 'red';
+    localStorage.setItem('city', '');    
+  }
 }
 
 function setCity(event) {
