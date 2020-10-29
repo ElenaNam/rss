5773b05ba6ff29c2cf3ada2a5863af70c890408e
ElenaNam <zaidelsonelena@gmail.com>
2020-10-30 02:03:15 +0400

    feat:add sound for normal keys

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -1,17 +1,22 @@
 const Keyboard = {
     elements: {
-        main: null, 
-        keysContainer: null,
-        keys:[]
+        main: null, //не существует
+        keysContainer: null, //не существует
+        keys:[]        
     },
+
+    //обработчики событий
     eventHandlers: {
-        oninput: null,
-        onclose: null
+        oninput: null,  //когда элемент получает ввод данных от пользователя
+        onclose: null   //событие закрытия
     },
+
     properties: {
         value: "",
-        capsLock: false
+        capsLock: false,
+        //sound: null
     },
+    
     init() {
         //создаем элементы
         this.elements.main = document.createElement('div');
@@ -22,6 +27,8 @@ const Keyboard = {
         this.elements.keysContainer.appendChild(this._createKeys());
 
         this.elements.keys = this.elements.keysContainer.querySelectorAll('.keyboard__key');
+        //this.elements.sound = this.elements.document.getElementById('music');
+        
 
         //добавляем в DOM
         this.elements.main.appendChild(this.elements.keysContainer);
@@ -31,12 +38,16 @@ const Keyboard = {
         document.querySelectorAll('.use-keyboard-input').forEach(element => {
             element.addEventListener('focus',() => {
                 this.open(element.value, currentValue =>{
+                    
                     element.value = currentValue;
+                    
                 });
             });
         });
 
     },
+    // создать клавиши
+
     _createKeys() {
         const fragment = document.createDocumentFragment();
         const keyLayout = [
@@ -46,6 +57,15 @@ const Keyboard = {
             "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
             "space"
           ];
+        const keyLayoutRu = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з","х","ъ", 
+            "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "enter",
+            "done", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
+            "space"
+          ];
+        
+        
 
         //Create HTML for an icon
         const createIconHTML = (icon_name) => {
@@ -54,11 +74,13 @@ const Keyboard = {
 
         keyLayout.forEach(key => {
             const keyElement = document.createElement('button');
-            const insertLineBreak = ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;
-
+            const insertLineBreak = ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;  
+           
+            
             //добавить атрибуты/классы
-            keyElement.setAttribute('type', 'button');
-            keyElement.classList.add('keyboard__key'); //стиль
+            keyElement.setAttribute('type', 'button'); //type="button"
+            keyElement.classList.add('keyboard__key'); 
+            //keyElement.setAttribute('data-key', 'letter');
 
             switch (key) {
                 case 'backspace': 
@@ -89,7 +111,7 @@ const Keyboard = {
 
                     keyElement.addEventListener('click', () => {
                         this.properties.value += '\n';
-                        this._triggerEvent('oninput');                       
+                        this._triggerEvent('oninput');     //для физ.клав                  
                     })
                     
                     break;
@@ -116,16 +138,17 @@ const Keyboard = {
                     
                     break;    
 
-                default: 
+                default: // при клике на букву или цифру
                     keyElement.textContent = key.toLowerCase();
-
                     keyElement.addEventListener('click', () => {
                         this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
-                        this._triggerEvent('oninput');                       
+                        this._triggerEvent('oninput');                    
+                        const normalSound = document.getElementById('normal');                    
+                        normalSound.play();           
                     })
                     
-                    break;  
-
+                    break;                               
+     
             }
             fragment.appendChild(keyElement);
             if (insertLineBreak) {
@@ -137,7 +160,7 @@ const Keyboard = {
 
     },
 
-    _triggerEvent(handlerName) {
+    _triggerEvent(handlerName) {        //handlerName - это oninput/onclose
         if (typeof this.eventHandlers[handlerName] == 'function') {
             this.eventHandlers[handlerName](this.properties.value);
         }
@@ -156,18 +179,20 @@ const Keyboard = {
 
         console.log('Caps Lock Toggled!');
     },
+    //выезжает клавиатура
     open(initialValue, oninput, onclose) {
         this.properties.value = initialValue || '';
         this.eventHandlers.oninput = oninput;
         this.eventHandlers.onclose = onclose;
-        this.elements.main.classList.remove('keyboard--hidden');
+        this.elements.main.classList.remove('keyboard--hidden'); //покажи
 
     },
+    //прячется клавиатура
     close() {
         this.properties.value = '';
         this.eventHandlers.oninput = oninput;
         this.eventHandlers.onclose = onclose;
-        this.elements.main.classList.add('keyboard--hidden');
+        this.elements.main.classList.add('keyboard--hidden'); //спрячь
 
     },
 
@@ -181,3 +206,11 @@ window.addEventListener("DOMContentLoaded", function() {
         console.log('keyboard closed! Finishing value: ' + currentValue);  
     });*/
 });
+
+
+                        
+
+
+
+
+/*his.elements.keys.addEventListener('click', play());*/
