53bde99d8f366a0b615f4e191230e7ab07256bf2
ElenaNam <zaidelsonelena@gmail.com>
2020-11-10 16:22:12 +0400

    feat: upgrade structure script

--- codejam-the-gem-puzzle/src/index.js
+++ codejam-the-gem-puzzle/src/index.js
@@ -1,23 +1,36 @@
-const Puzzle = {
+const wrapper = document.createElement('div'); //фон
+const puzzleWrapper = document.createElement('div');//поле
 
-    properties: {
-        value: ''
+//const cellContainer = document.createElement('div'); //строка
+const fragment = document.createDocumentFragment();
 
-    },
+/*let cells = [
+    ['1','2','3','4'],
+    ['5','6','7','8'],
+    ['9','10','11','12'],
+    ['13','14','15']
+  ];*/
 
-    init() {
-        //фон
-        const wrapper = document.createElement('div');
+let cells = [
+    '1','2','3','4',
+    '5','6','7','8',
+    '9','10','11','12',
+    '13','14','15'
+];
+
+function init() {
+        //фон        
         wrapper.classList.add('wrapper');
         document.body.appendChild(wrapper);
 
-        //поле
-        const puzzleWrapper = document.createElement('div');
+        //поле        
         puzzleWrapper.classList.add('puzzle-wrapper');
         document.body.appendChild(puzzleWrapper);
 
-        //клетки    
-        puzzleWrapper.appendChild(this.createCells());
+        //клетки
+           
+        //puzzleWrapper.appendChild(createCells());
+        puzzleWrapper.appendChild(createCells());
 
         //анимация фона
        /* const blinkBg = () => {
@@ -31,73 +44,96 @@ const Puzzle = {
         }*/
 
 
-    },
+};
 
-    createCells() {
-        const fragment = document.createDocumentFragment();
-        let cells = [
-          ['1','2','3','4'],
-          ['5','6','7','8'],
-          ['9','10','11','12'],
-          ['13','14','15','']
-        ];
-        
-        /*let cells = [
-            '1','2','3','4',
-            '5','6','7','8',
-            '9','10','11','12',
-            '13','14','15',''
-        ];*/
-
-        cells.forEach(cell => {
-            //const cellElement = document.createElement('div');
-            const cellContainer = document.createElement('div'); 
-            cellContainer.classList.add('cell-container');
-            
-                for (let j = 0; j < cell.length; j++){
-                    const cellElement = document.createElement('div'); 
-                    cellElement.textContent = cell[j];
-                    
-                    if (cell[j].length > 0){
-                        cellElement.classList.add('puzzle-cell'); 
-                    } else {
-                        cellElement.classList.add('puzzle-cell--empty'); 
-                    }
-                    cellContainer.appendChild(cellElement); 
-
-                }
+function createCells() {          
 
 
-            
+    cells.forEach(cell => {           
+             
+
+                    const cellElement = document.createElement('div'); //клетка
+                    cellElement.classList.add('puzzle-cell');
+                    cellElement.textContent = cell;
+                        
+                        
+                    //разрешить перетаскивание мышью
+                    cellElement.setAttribute('draggable', 'true'); 
+                    //                     
+                    cellElement.addEventListener('dragstart', function(){
+                            //                            
+                        setTimeout(() => {
+                            this.style.display = 'none';
+                        }, 0)
+                            
+                    });
+
+                    cellElement.addEventListener('dragend', function(){
+                            //
+                        setTimeout(() => {
+                            this.style.display = 'flex';
+                        }, 0)
+                            
+                    });
+
+                        //когда пятнашка находится над пустым местом
+                    puzzleWrapper.addEventListener('dragover', function(e){
+                        e.preventDefault();
+                        console.log('dragover');
+                    }); 
+                                
+                        //
+                    puzzleWrapper.addEventListener('drop', function(){
+                        //cellContainer.appendChild(cell[j]);
+                        puzzleWrapper.appendChild(cell);
+                        console.log('drop');
+                    }); 
+
+                        //КЛИК
+                    cellElement.addEventListener('click', function(){
+                            //
+                        cellElement.classList.remove('puzzle-cell');
+                        
+                            
+                    });
+
+                                                   
+                        
+                    /*} else {
+                        cellElement.classList.add('puzzle-cell--empty');
+                     
+                        //когда пятнашка находится над пустым местом
+                        cellElement.addEventListener('dragover', function(e){
+                            e.preventDefault();
+                            console.log('dragover');
+                        }); 
+
+                        //
+                        cellElement.addEventListener('drop', function(){
+                            cellContainer.appendChild(cell[j]);
+                            console.log('drop');
+                        }); 
+
+                    }*/
+                    //cellContainer.appendChild(cellElement); 
+            //puzzleWrapper.appendChild(cell); 
+
+               // }        
                 
-               
+ 
+            //fragment.appendChild(cellContainer);  
+            fragment.appendChild(cellElement);
+
+    })        
+    return fragment;
+};
 
 
-                
 
-/*
-                cellElement.textContent = cell; 
-                //проверка на пустую строку
-                if (cell){
-                    cellElement.classList.add('puzzle-cell'); 
-                } else {
-                    cellElement.classList.add('puzzle-cell--empty'); 
-                }
- */            
-            
-            
+    
 
-                              
-          
-            
-
-            fragment.appendChild(cellContainer);  
-
-        })        
-        return fragment;
-    }
-}
 
 window.addEventListener("DOMContentLoaded", function() {
-    Puzzle.init();
+    init();
+    createCells(); 
 });
