019b4f23af6aa86c8be7ba29a1e5da67ec726bf5
ElenaNam <zaidelsonelena@gmail.com>
2020-11-10 21:22:41 +0400

    feat: add movement on click

--- codejam-the-gem-puzzle/src/index.js
+++ codejam-the-gem-puzzle/src/index.js
@@ -1,5 +1,7 @@
 const wrapper = document.createElement('div'); //фон
 const puzzleWrapper = document.createElement('div');//поле
+const cellSize = 95; //размер клетки
+const cellElement = document.createElement('div'); //клетка   
 
 //const cellContainer = document.createElement('div'); //строка
 const fragment = document.createDocumentFragment();
@@ -11,12 +13,14 @@ const fragment = document.createDocumentFragment();
     ['13','14','15']
   ];*/
 
-let cells = [
+/*let cells = [
     '1','2','3','4',
     '5','6','7','8',
     '9','10','11','12',
-    '13','14','15'
-];
+    '13','14','15',''
+];*/
+
+
 
 function init() {
         //фон        
@@ -46,21 +50,73 @@ function init() {
 
 };
 
-function createCells() {          
+function createCells() {
+    const cells = [];
+    const empty = {
+    top: 0,
+    left: 0
+    };  
+    cells.push(empty);
+
+    function move (index) {
+        const cell = cells[index];
+
+        //ищем разницу с коорд.пустой клетки
+        const leftVar = Math.abs(empty.left - cell.left);
+        const topVar = Math.abs(empty.top - cell.top);
+        //если рядом пустой клетки нет, ничего не делай
+        if (leftVar + topVar > 1) {
+            return;
+        }
+
+        
+        cell.element.style.left = `${empty.left * cellSize}px`;
+        cell.element.style.top = `${empty.top * cellSize}px`;
+        //в промеж.переменные записываем коорд-ты пустой клетки
+        const emptyLeft = empty.left;
+        const emptyTop = empty.top;
+        //в коорд-ты пустой клетки записываем коорд-ты текущей клетки
+        empty.left = cell.left;
+        empty.top = cell.top;
+        //в коорд-ты текущей клетки записываем коорд-ты пустой
+        cell.left = emptyLeft;
+        cell.top = emptyTop;
+
+    }
+    
+    for (let i = 1; i < 16; i++) {
+        const cellElement = document.createElement('div'); //клетка                    
+        cellElement.textContent = i;
+        cellElement.classList.add('puzzle-cell');
+        //разрешить перетаскивание мышью
+        cellElement.setAttribute('draggable', 'true'); 
+
+        const left = i % 4;
+        const top = (i - left) / 4;
+
+        cells.push ({
+            left: left,
+            top: top,
+            element: cellElement
+        });
 
 
-    cells.forEach(cell => {           
-             
+        cellElement.style.left = `${left * cellSize}px`;
+        cellElement.style.top = `${top * cellSize}px`;
 
-                    const cellElement = document.createElement('div'); //клетка
-                    cellElement.classList.add('puzzle-cell');
-                    cellElement.textContent = cell;
-                        
-                        
-                    //разрешить перетаскивание мышью
-                    cellElement.setAttribute('draggable', 'true'); 
+        puzzleWrapper.append(cellElement);
+
+        //КЛИК
+        cellElement.addEventListener('click', () => {
+        //
+        //cellElement.classList.remove('puzzle-cell');
+        move(i);     
+                               
+        });
+    };
+
                     //                     
-                    cellElement.addEventListener('dragstart', function(){
+                    /*cellElement.addEventListener('dragstart', function(){
                             //                            
                         setTimeout(() => {
                             this.style.display = 'none';
@@ -85,55 +141,19 @@ function createCells() {
                         //
                     puzzleWrapper.addEventListener('drop', function(){
                         //cellContainer.appendChild(cell[j]);
-                        puzzleWrapper.appendChild(cell);
+                        //puzzleWrapper.appendChild(cellElement);
                         console.log('drop');
                     }); 
-
-                        //КЛИК
-                    cellElement.addEventListener('click', function(){
-                            //
-                        cellElement.classList.remove('puzzle-cell');
-                        
-                            
-                    });
-
-                                                   
-                        
-                    /*} else {
-                        cellElement.classList.add('puzzle-cell--empty');
-                     
-                        //когда пятнашка находится над пустым местом
-                        cellElement.addEventListener('dragover', function(e){
-                            e.preventDefault();
-                            console.log('dragover');
-                        }); 
-
-                        //
-                        cellElement.addEventListener('drop', function(){
-                            cellContainer.appendChild(cell[j]);
-                            console.log('drop');
-                        }); 
-
-                    }*/
-                    //cellContainer.appendChild(cellElement); 
-            //puzzleWrapper.appendChild(cell); 
-
-               // }        
-                
- 
-            //fragment.appendChild(cellContainer);  
-            fragment.appendChild(cellElement);
-
-    })        
-    return fragment;
-};
+*/
 
 
-
-    
+  
+        fragment.appendChild(cellElement);
+        return fragment;
+};       
+      
 
 
 window.addEventListener("DOMContentLoaded", function() {
-    init();
-    createCells(); 
+    init();    
 });
