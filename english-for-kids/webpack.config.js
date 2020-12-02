0c58e943b2227ae996e510065d026c8ae8f3cd80
ElenaNam <zaidelsonelena@gmail.com>
2020-12-02 16:34:04 +0400

    init: start task

---
+++ english-for-kids/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {test: /\.svg$/, use: 'svg-inline-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}            
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        overlay: true,
+        open: true,
+        port: 8080,
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
