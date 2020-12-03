3cf9621d8533d4793c02a58b968255a0d28f75da
ElenaNam <zaidelsonelena@gmail.com>
2020-12-03 18:50:14 +0400

    feat: add hamburger-menu

---
+++ english-for-kids/src/components/header.js
@@ -0,0 +1,50 @@
+const { document } = global;
+const renderHeader = () => {
+  const headerWrapper = document.createElement('div');
+  headerWrapper.classList.add('header-wrapper');
+  document.body.appendChild(headerWrapper);
+
+  /* burger-menu */
+  const burgerWrapper = document.createElement('div');
+  burgerWrapper.classList.add('burger-wrapper');
+  headerWrapper.appendChild(burgerWrapper);
+
+  const burgerMenuBtn = document.createElement('a');
+  burgerMenuBtn.classList.add('burger-menu_button');
+  burgerWrapper.appendChild(burgerMenuBtn);
+
+  const lines = document.createElement('span');
+  lines.classList.add('burger-menu_lines');
+  burgerMenuBtn.appendChild(lines);
+
+  const nav = document.createElement('nav');
+  nav.classList.add('burger-nav');
+  burgerWrapper.appendChild(nav);
+
+  const menuLinks = ['Home page', 'Action (set A)', 'Action (set B)', 'Action (set C)', 'Adjective', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotion'];
+  menuLinks.forEach((link) => {
+    const navLink = document.createElement('a');
+    navLink.classList.add('burger-nav_link');
+    navLink.setAttribute('href', '#');
+    navLink.textContent = link;
+    nav.appendChild(navLink);
+  });
+
+  const burgerOverlay = document.createElement('div');
+  burgerOverlay.classList.add('burger-menu_overlay');
+  burgerWrapper.appendChild(burgerOverlay);
+
+  burgerMenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    burgerWrapper.classList.toggle('burger-menu_active');
+    document.body.classList.toggle('lock');
+  });
+  burgerOverlay.addEventListener('click', () => {
+    burgerWrapper.classList.toggle('burger-menu_active');
+  });
+  nav.addEventListener('click', (e) => {
+    e.preventDefault();
+    burgerWrapper.classList.remove('burger-menu_active');
+  });
+};
+export default renderHeader;
--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -1,29 +1,38 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin'); 
 const path = require('path');
 
+
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {test: /\.svg$/, use: 'svg-inline-loader'},
-            {test: /\.css$/, use: ['style-loader', 'css-loader']},
-            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}            
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {test: /\.svg$/, use: 'svg-inline-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.(png|jpe?g|gif|ttf|woff|woff2|eot)$/, loader: 'file-loader', options: {publicPath: 'dist/assets/img'}},
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        overlay: true,
-        open: true,
-        port: 8080,
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/assets/image", to: "img" },
+        { from: "src/assets/audio", to: "audio" },
+      ],
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    overlay: true,
+    open: true,
+    port: 8080,
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 }
