62e5cd7d65e4c9cee8710c9d6ada9ed24918275d
ElenaNam <zaidelsonelena@gmail.com>
2020-12-21 16:12:36 +0400

    feat: add media requests

--- covid-dashboard/src/components/mainPage.js
+++ covid-dashboard/src/components/mainPage.js
@@ -11,10 +11,16 @@ const renderMainPage = async () => {
     //console.log(mapWrapper);
     renderMap();
     mainPageWrapper.appendChild(mapWrapper);
-    renderTable();
-    mainPageWrapper.appendChild(table);
+
+    const column2 = document.createElement('div');
+    column2.classList.add('column-2');  
+
+
+    renderTable();    
+    column2.appendChild(table);
     getListCountries();
-    mainPageWrapper.appendChild(listWrapper);
+    column2.appendChild(listWrapper);
+    mainPageWrapper.appendChild(column2);
 
 }
 
